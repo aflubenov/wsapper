f67a6bd7515e04bd46c5abfe5316b6f11373baed
Angel <angelfernando@gmail.com>
2019-09-13 17:37:39 -0300

    holis

---
+++ lanzachrome.sh
@@ -0,0 +1,2 @@
+/opt/google/chrome/google-chrome --disable-site-isolation-trials  --disable-web-security --disable-gpu --allow-file-access-from-files  --user-data-dir=./datatemporal/
+
---
+++ wsapero.js
@@ -0,0 +1,94 @@
+let esperaParaClick = 5000;
+
+let fnEnviaelMensaje = () => {
+    let elMensaje = document.getElementById('theMensaje').value;
+    let losTelefonosElemento = document.getElementById('theTelefonos');
+    let losTelefonos = losTelefonosElemento.value;
+    let elTelefono = "";
+    let elIframe = document.getElementById('theWsappIframe');
+    let laUrl = "https://web.whatsapp.com/send?"; //phone="5493415473805&text=I%27m%20interested%20in%20your%20car%20for%20sale&source=&data=
+
+
+    if (!losTelefonos) {
+        alert('Fin!');
+        return;
+    }
+
+    losTelefonos = losTelefonos.split('\n');
+    if (losTelefonos.length == 0) {
+        alert('Fin!');
+        return;
+    }
+
+    elTelefono = losTelefonos.shift();
+    losTelefonos = losTelefonos.join('\n');
+    losTelefonosElemento.value = losTelefonos;
+
+    laUrl = laUrl + "phone=" + elTelefono;
+    laUrl = laUrl + "&text=" + encodeURIComponent(elMensaje);
+    laUrl = laUrl + "&source=&data="
+
+    elIframe.src = laUrl;
+
+    setTimeout(() => {
+        intentarEnviarMensaje(elIframe);
+    }, esperaParaClick);
+}
+
+
+let intentarEnviarMensaje = (pIframe) => {
+    let elDocumento = pIframe.contentDocument;
+
+    if (!elDocumento) {
+        setTimeout(() => {
+            intentarEnviarMensaje(pIframe);
+        }, esperaParaClick);
+
+        return;
+    }
+
+    let elFooter = elDocumento.getElementsByTagName("footer");
+    if (!elFooter) {
+        setTimeout(() => {
+            intentarEnviarMensaje(pIframe);
+        }, esperaParaClick);
+
+        return;
+    }
+
+    elFooter = elFooter[0];
+    if (!elFooter) {
+        setTimeout(() => {
+            intentarEnviarMensaje(pIframe);
+        }, esperaParaClick);
+
+        return;
+    }
+
+    let elBoton = elFooter.getElementsByClassName('_3M-N-');
+    if (!elBoton) {
+        setTimeout(() => {
+            intentarEnviarMensaje(pIframe);
+        }, esperaParaClick);
+
+        return;
+    }
+
+    elBoton = elBoton[0];
+    if (!elBoton) {
+        setTimeout(() => {
+            intentarEnviarMensaje(pIframe);
+        }, esperaParaClick);
+
+        return;
+    }
+
+    elBoton.click();
+
+    setTimeout(() => {
+        fnEnviaelMensaje();
+    }, esperaParaClick * 3);
+}
+
+
+
